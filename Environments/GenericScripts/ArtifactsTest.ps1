8afb0d468178f5159d222fa9a38a18d30674f1d2
OmegaMadLab <locurtola@tin.it>
2018-04-03 15:40:38 +0200

    Artifact update

--- Environments/GenericScripts/ArtifactsTest.ps1
+++ Environments/GenericScripts/ArtifactsTest.ps1
@@ -9,5 +9,21 @@ $FullVMId = "/subscriptions/$SubscriptionId/resourceGroups/$resourceGroupName/pr
  
 $virtualMachine = Get-AzureRmResource -ResourceId $FullVMId -ODataQuery '$expand=Properties($expand=Artifacts)' -ExpandProperties
 
+$props = $virtualMachine | Select Properties 
+
+$subItems = Get-AzureRmResource | Where-Object {$_.ResourceGroupName -eq $props.Properties.computeId.Split('/')[4]} 
 
 $virtualMachine.Properties
+
+$NIC = $subItems | Where-Object ResourceType -eq Microsoft.Network/networkInterfaces | Get-AzureRmNetworkInterface
+
+$nsg = Get-AzureRmNetworkSecurityGroup -ResourceGroupName 'SJ-Obinu-RDPTest2' -Name 'dtl-DC-NSG'
+
+$NIC.NetworkSecurityGroup = $nsg
+$NIC | Set-AzureRmNetworkInterface
+
+$cse = $subItems | Where-Object ResourceType -eq Microsoft.Compute/virtualMachines/extensions 
+
+$script = Get-AzureRmVMCustomScriptExtension -ResourceGroupName $cse.ResourceGroupName -Name ($cse.Name.Split('/')[1]) -vmName $virtualMachineName
+
+$script.
