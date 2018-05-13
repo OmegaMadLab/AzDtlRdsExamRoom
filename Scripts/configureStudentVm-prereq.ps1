99d6c6cd8e79c8990ea727a17023e3b61a51df7a
OmegaMadLab <locurtola@tin.it>
2018-05-13 21:55:26 +0200

    StudentConfigUpdate

--- Scripts/configureStudentVm-prereq.ps1
+++ Scripts/configureStudentVm-prereq.ps1
@@ -1,4 +1,15 @@
+[CmdletBinding()]
+
+param (
+    [string]$TeacherVmIp
+)
+
 Import-Module ServerManager
+
+#Add entry for ExamRoom = Teacher VM IP in hosts
+$Hosts = Get-Item "$env:SYSTEMROOT\system32\Drivers\etc\hosts"
+$Hosts | Add-Content -value "`nExamRoom`t$TeacherVmIp"
+
 
 #Install RDS Features
 Install-WindowsFeature -Name 'RDS-RD-Server' -IncludeAllSubFeature -IncludeManagementTools -Restart
