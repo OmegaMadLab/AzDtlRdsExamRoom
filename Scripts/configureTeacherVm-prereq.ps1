5794d2b79b113eba970b5e9ad8cf3c54d68e6955
OmegaMadLab <locurtola@tin.it>
2018-07-19 19:39:10 +0200

    PrereqReview

--- Scripts/configureTeacherVm-prereq.ps1
+++ Scripts/configureTeacherVm-prereq.ps1
@@ -6,62 +6,37 @@ param(
 
 Import-Module ServerManager
 
-Write-Output "Enabling CredSSP..."
-Enable-WSManCredSSP -Role Client -DelegateComputer "*.$DomainName" -Force
+$delegate = "*.$DomainName"
+$wsman    = "WSMAN/*.$DomainName"
+
+Write-Output "Starting enabling CredSSP" | Out-File -FilePath 'C:\WINDOWS\Temp\rds_deployment.log' -Append	
+Enable-WSManCredSSP -Role Client -DelegateComputer $delegate -Force
 Enable-WSManCredSSP -Role Server -Force
-Write-Output "CredSSP enabled."
+Write-Output "CredSSP enabled" | Out-File -FilePath 'C:\WINDOWS\Temp\rds_deployment.log' -Append
+	
+Write-Output "Set TrustedHosts" | Out-File -FilePath 'C:\WINDOWS\Temp\rds_deployment.log' -Append
+Set-item wsman:localhost\client\trustedhosts -value $delegate -Force
+Write-Output "TrustedHosts setup completed" | Out-File -FilePath 'C:\WINDOWS\Temp\rds_deployment.log' -Append
 
-# $allowed = @("WSMAN/*.$DomainName")
-
-# $key = 'hklm:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation'
-# if (!(Test-Path $key)) {
-#     mkdir $key
-# }
-# New-ItemProperty -Path $key -Name AllowFreshCredentials -Value 1 -PropertyType Dword -Force            
-
-# $key = Join-Path $key 'AllowFreshCredentials'
-# if (!(Test-Path $key)) {
-#     mkdir $key
-# }
-# $i = 1
-# $allowed | ForEach-Object {
-#     # Script does not take into account existing entries in this key
-#     New-ItemProperty -Path $key -Name $i -Value $_ -PropertyType String -Force
-#     $i++
-# }
+Write-Output "Enable CredSSP Fresh NTLM Only" | Out-File -FilePath 'C:\WINDOWS\Temp\rds_deployment.log' -Append
+New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name AllowFreshCredentialsWhenNTLMOnly -Value 1 -PropertyType DWORD -Force | Out-Null
+New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name ConcatenateDefaults_AllowFreshNTLMOnly -Value 1 -PropertyType DWORD -Force | Out-Null
+New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name AllowFreshCredentialsWhenNTLMOnly -Force | Out-Null
+New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name 1 -Value $wsman -PropertyType String -Force | Out-Null
+Write-Output "CredSSP Fresh NTLM Only enabled" | Out-File -FilePath 'C:\WINDOWS\Temp\rds_deployment.log' -Append
 
 #Install RDS Features
-Write-Output "Installing RDS components..."
+Write-Output "Installing RDS components..." | Out-File -FilePath 'C:\WINDOWS\Temp\rds_deployment.log' -Append
 Install-WindowsFeature -Name 'RDS-Connection-Broker', 'RDS-Licensing', 'RDS-Web-Access', 'RSAT-RDS-Tools' -IncludeAllSubFeature -IncludeManagementTools 
+Write-Output "RDS Components installed" | Out-File -FilePath 'C:\WINDOWS\Temp\rds_deployment.log' -Append
 
-Write-Output "Enabling PSRemoting..."
+Write-Output "Enabling PSRemoting..." | Out-File -FilePath 'C:\WINDOWS\Temp\rds_deployment.log' -Append
 Enable-PSRemoting -Force -verbose
+Write-Output "PSRemoting Enabled" | Out-File -FilePath 'C:\WINDOWS\Temp\rds_deployment.log' -Append
 
+Write-Output "Restarting..." | Out-File -FilePath 'C:\WINDOWS\Temp\rds_deployment.log' -Append
 Restart-Computer -Force -Confirm:$false
 
-#Check for pending reboot, and eventually restart server
-#Adapted from https://gist.github.com/altrive/5329377
-#Based on <http://gallery.technet.microsoft.com/scriptcenter/Get-PendingReboot-Query-bdb79542>
-# function Test-PendingReboot
-# {
-#  if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { return $true }
-#  if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { return $true }
-#  if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { return $true }
-#  try { 
-#    $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
-#    $status = $util.DetermineIfRebootPending()
-#    if(($status -ne $null) -and $status.RebootPending){
-#      return $true
-#    }
-#  }catch{}
- 
-#  return $false
-# }
-
-# if(Test-PendingReboot) {
-#     Write-Verbose "There's a pending reboot. Restarting server..."
-#     Restart-Computer -Force -Confirm:$false
-# }
 
 
 
