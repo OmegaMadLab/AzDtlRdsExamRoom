681457a880ca90d42364157bd0445b8f8e11c3c8
OmegaMadLab <locurtola@tin.it>
2018-07-19 19:28:50 +0200

    TestCSE

--- Scripts/configureTeacherVm-prereq.ps1
+++ Scripts/configureTeacherVm-prereq.ps1
@@ -6,27 +6,29 @@ param(
 
 Import-Module ServerManager
 
+Write-Output "Enabling CredSSP..."
 Enable-WSManCredSSP -Role Client -DelegateComputer "*.$DomainName" -Force
 Enable-WSManCredSSP -Role Server -Force
+Write-Output "CredSSP enabled."
 
-$allowed = @("WSMAN/*.$DomainName")
+# $allowed = @("WSMAN/*.$DomainName")
 
-$key = 'hklm:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation'
-if (!(Test-Path $key)) {
-    mkdir $key
-}
-New-ItemProperty -Path $key -Name AllowFreshCredentials -Value 1 -PropertyType Dword -Force            
+# $key = 'hklm:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation'
+# if (!(Test-Path $key)) {
+#     mkdir $key
+# }
+# New-ItemProperty -Path $key -Name AllowFreshCredentials -Value 1 -PropertyType Dword -Force            
 
-$key = Join-Path $key 'AllowFreshCredentials'
-if (!(Test-Path $key)) {
-    mkdir $key
-}
-$i = 1
-$allowed | ForEach-Object {
-    # Script does not take into account existing entries in this key
-    New-ItemProperty -Path $key -Name $i -Value $_ -PropertyType String -Force
-    $i++
-}
+# $key = Join-Path $key 'AllowFreshCredentials'
+# if (!(Test-Path $key)) {
+#     mkdir $key
+# }
+# $i = 1
+# $allowed | ForEach-Object {
+#     # Script does not take into account existing entries in this key
+#     New-ItemProperty -Path $key -Name $i -Value $_ -PropertyType String -Force
+#     $i++
+# }
 
 #Install RDS Features
 Write-Output "Installing RDS components..."
