fb273f8c4d9f2384387b4faba432317c50cbf7a4
OmegaMadLab <locurtola@tin.it>
2018-07-22 00:38:46 +0200

    test new

--- Scripts/TestWMF.ps1
+++ Scripts/TestWMF.ps1
@@ -1,1 +1,3 @@
-Install-Module AzureAD
+Install-Module AzureAD -SkipPublisherCheck -Force -Confirm:$false
+Install-PackageProvider -Name NuGet -Force -MinimumVersion 2.8.5.201
+Install-Module -Name NTFSSecurity -Force -Confirm:$false
