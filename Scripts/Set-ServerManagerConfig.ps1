b27279ad58b13b28676ff585df5c4dfe9febb401
OmegaMadLab <locurtola@tin.it>
2018-07-23 14:47:23 +0200

    Char correction

--- Scripts/Set-ServerManagerConfig.ps1
+++ Scripts/Set-ServerManagerConfig.ps1
@@ -1,8 +1,8 @@
-get-process ServerManager | stop-process –force
+get-process ServerManager | stop-process -Force
 
 if(-not (Test-Path "$env:APPDATA\Microsoft\Windows\ServerManager")) {
     New-Item -Path "$env:APPDATA\Microsoft\Windows\ServerManager" -ItemType Directory
 }
 Copy-Item "C:\ServerManagerConfig\ServerList.xml" -Destination "$env:APPDATA\Microsoft\Windows\ServerManager\ServerList.xml" -Force -Confirm:$false
 
-start-process –filepath $env:SystemRoot\System32\ServerManager.exe –WindowStyle Maximized
+start-process -FilePath $env:SystemRoot\System32\ServerManager.exe -WindowStyle Maximized
