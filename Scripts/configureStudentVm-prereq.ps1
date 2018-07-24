1a7f973fa879ed69ad0aa6bc02b990b41b018de3
OmegaMadLab <locurtola@tin.it>
2018-07-24 16:30:30 +0200

    Hosts correction

--- Scripts/configureStudentVm-prereq.ps1
+++ Scripts/configureStudentVm-prereq.ps1
@@ -15,7 +15,7 @@ Add-LocalGroupMember -Group "Administrators" -Member $DomainAdminName
 
 #Add entry for ExamRoom = Teacher VM IP in hosts
 $Hosts = Get-Item "$env:SYSTEMROOT\system32\Drivers\etc\hosts"
-$Hosts | Add-Content -value "`nExamRoom`t$TeacherVmIp"
+$Hosts | Add-Content -value "`n$TeacherVmIp`tExamRoom"
 
 
 #Install RDS Features
